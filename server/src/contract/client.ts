a43d9a8ce7c53bf4bbe2147b9b1b9c294baa7c4e
PratikshaGayen <pratikshagayen35118@gmail.com>
2026-05-25 20:27:26 +0530

    feat: deploy to Testnet Bradbury

--- server/src/contract/client.ts
+++ server/src/contract/client.ts
@@ -2,7 +2,7 @@
  * Server-side GenLayer client.
  */
 import { createClient, createAccount, generatePrivateKey } from "genlayer-js";
-import { studionet } from "genlayer-js/chains";
+import { testnet_bradbury } from "genlayer-js/chains";
 import { TransactionStatus } from "genlayer-js/types";
 
 let cached: ReturnType<typeof createClient> | null = null;
@@ -16,7 +16,7 @@ function getClient() {
   const account = createAccount(pk);
   console.log(`[contract] server address: ${account.address}`);
 
-  const client = createClient({ chain: studionet, endpoint: RPC, account });
+  const client = createClient({ chain: testnet_bradbury, endpoint: RPC, account });
 
   const c = client as unknown as { request: (args: { method: string }) => Promise<unknown> };
   const orig = c.request.bind(client);
